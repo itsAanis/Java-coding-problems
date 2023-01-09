89950d4876f5766b0bafc2e0443d5f84c1ce1b12
Aanis <aanishassan@hotmail.com>
2023-01-09 13:25:35 +0000

    first 8kyu codewars problem

---
+++ EvenOrOdd.java
@@ -0,0 +1,14 @@
+
+/*Create a function that takes an integer as an argument and
+ returns "Even" for even numbers or "Odd" for odd numbers.    */
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        String odd = "Odd";
+        String even = "Even";
+        if ( number % 2 == 0 ) {return even;}
+        else {return odd;}
+
+    }
+
+}
